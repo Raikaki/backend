7ac2a2f2c5e1b2f5f8c4fecfcde6c3e24951eab7
Raikaki <hcdat862002@gmail.com>
2024-06-21 14:11:56 +0700

    Update SecurityConfig.java

--- back-end/src/main/java/com/animeweb/security/SecurityConfig.java
+++ back-end/src/main/java/com/animeweb/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
             "/static/imgs/**",
             "/servicePack/**",
             "/comment/**",
-            "/login-google"
+            "/login/google"
 
     };
     @Bean
@@ -80,7 +80,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("*"); // Cho phép truy cập từ tất cả các nguồn
         configuration.addAllowedMethod("*"); // Cho phép tất cả các phương thức (GET, POST, PUT, DELETE, v.v.)
         configuration.addAllowedHeader("*"); // Cho phép tất cả các tiêu đề
-        configuration.setAllowedOrigins(List.of("https://animewebnew.netlify.app/"));
+        configuration.setAllowedOrigins(List.of("https://animewebnew.netlify.app"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
