96420f464a54c0df3894f68219b12656580df7c6
Raikaki <hcdat862002@gmail.com>
2024-06-22 22:53:04 +0700

    tui tai` z tr

--- back-end/src/main/java/com/animeweb/security/SecurityConfig.java
+++ back-end/src/main/java/com/animeweb/security/SecurityConfig.java
@@ -108,8 +108,9 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("*"); // Cho phép truy cập từ tất cả các nguồn
         configuration.addAllowedMethod("*"); // Cho phép tất cả các phương thức (GET, POST, PUT, DELETE, v.v.)
         configuration.addAllowedHeader("*"); // Cho phép tất cả các tiêu đề
-//        configuration.setAllowedOrigins(List.of("https://animewebnew.netlify.app","https://backend-wpxi.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://animewebnew.netlify.app","https://backend-wpxi.onrender.com"));
         configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
